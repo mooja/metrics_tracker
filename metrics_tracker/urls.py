0c6f99a562bbaf9ccc370ae8beeaaa157ebc5bba
Max Shkurygin <max.atreides@gmail.com>
2020-12-22 15:36:59 -0500

    added a month plot

--- metrics_tracker/urls.py
+++ metrics_tracker/urls.py
@@ -1,24 +1,10 @@
-"""metrics_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
 from tracker import views
 
 urlpatterns = [
     path("", views.index, name="home"),
     path("plots/week/", views.week_plot, name="week_plot"),
+    path("plots/four_weeks/", views.four_week_plot, name="four_week_plot"),
     path("trackers/", views.tracker_detail, name="tracker_detail"),
     path("trackers/<int:id>/", views.tracker_detail, name="tracker_detail"),
     path("trackers/<int:id>/delete/", views.tracker_delete, name="tracker_delete"),
--- tracker/views.py
+++ tracker/views.py
@@ -13,6 +13,7 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.contrib import messages
 from django.core.cache import cache
+from django.db.models import Sum
 
 import numpy as np
 from matplotlib.figure import Figure
@@ -23,8 +24,13 @@ from tracker.forms import RecordForm, TrackerForm
 
 def index(request):
     trackers = Tracker.objects.all()  # pylint:disable=no-member
-    forms = (RecordForm(initial={'tracker': t.id}, fh_data={'tracker_id': t.id, 'index_view': True}) for t in Tracker.objects.all())  # pylint:disable=no-member
-    colors = cycle(('blue', 'green', 'red', 'purple'))
+    forms = (
+        RecordForm(
+            initial={"tracker": t.id}, fh_data={"tracker_id": t.id, "index_view": True}
+        )
+        for t in Tracker.objects.all()  #pylint:disable=no-member
+    )
+    colors = cycle(("blue", "green", "red", "purple"))
     trackers_and_forms_and_colors = zip(trackers, forms, colors)
     context = {
         "trackers": trackers,
@@ -37,7 +43,7 @@ def tracker_detail(request, id=None, delete=False):
     if request.method == "POST":
         form = TrackerForm(request.POST)
         if form.is_valid():
-            cache.delete('week_plot')
+            cache.delete("week_plot")
             if form.cleaned_data["action"] == "add":
                 form.save()
                 messages.success(request, "Tracker created.")
@@ -56,10 +62,11 @@ def tracker_detail(request, id=None, delete=False):
                 tracker.delete()
                 messages.success(request, "Tracker deleted.")
                 return redirect("home")
-            
+
             else:
-                return HttpResponse(f"Action {form.cleaned_data['action']} is not implemented.")
-    
+                return HttpResponse(
+                    f"Action {form.cleaned_data['action']} is not implemented."
+                )
 
     context = {}
     if not id:
@@ -67,10 +74,18 @@ def tracker_detail(request, id=None, delete=False):
     else:
         tracker = get_object_or_404(Tracker, id=id)
         context["form"] = TrackerForm(instance=tracker)
-        context["record_form"] = RecordForm(initial={'tracker': tracker.id}, fh_data={'index_view': False, 'tracker_id': id})
+        context["record_form"] = RecordForm(
+            initial={"tracker": tracker.id},
+            fh_data={"index_view": False, "tracker_id": id},
+        )
         context["record_forms"] = (
-            RecordForm(instance=r, fh_data={'index_view': False, 'tracker_id': id, 'record_id': r.id})
-            for r in Record.objects.all().filter(tracker__id=id).order_by('-date')  # pylint: disable=no-member
+            RecordForm(
+                instance=r,
+                fh_data={"index_view": False, "tracker_id": id, "record_id": r.id},
+            )
+            for r in Record.objects.all()  # pylint: disable=no-member
+            .filter(tracker__id=id)
+            .order_by("-date") 
         )
 
     return render(request, "tracker/tracker_detail.html", context)
@@ -79,7 +94,7 @@ def tracker_detail(request, id=None, delete=False):
 @require_POST
 def record_detail(request, tracker_id, record_id=None):
     tracker = get_object_or_404(Tracker, id=tracker_id)
-    fh_data = {'tracker_id': tracker.id, 'record_id': record_id}
+    fh_data = {"tracker_id": tracker.id, "record_id": record_id}
     if record_id:
         record = get_object_or_404(Record, id=record_id)
         form = RecordForm(request.POST, instance=record, fh_data=fh_data)
@@ -91,8 +106,9 @@ def record_detail(request, tracker_id, record_id=None):
         messages.error(request, f"Error in form submission: {form.errors}")
         return tracker_detail(request, id=tracker_id)
 
-    cache.delete('week_plot')
-    
+    cache.delete("week_plot")
+    cache.delete("four_week_plot")
+
     if form.cleaned_data["action"] == "add":
         record = Record(date=datetime.date.today(), tracker=tracker)
         form = RecordForm(request.POST, instance=record, fh_data=fh_data)
@@ -100,14 +116,14 @@ def record_detail(request, tracker_id, record_id=None):
         form.save()
         messages.success(request, "Added a new record.")
         return redirect("home")
-    
+
     if form.cleaned_data["action"] == "update":
         record = get_object_or_404(Record, id=record_id)
         form = RecordForm(request.POST, instance=record, fh_data=fh_data)
         form.is_valid()
         form.save()
         messages.success(request, "Updated record.")
-        return redirect(reverse("tracker_detail", kwargs={'id': tracker.id}))
+        return redirect(reverse("tracker_detail", kwargs={"id": tracker.id}))
 
     if form.cleaned_data["action"] == "delete":
         record = get_object_or_404(Record, id=record_id)
@@ -115,7 +131,7 @@ def record_detail(request, tracker_id, record_id=None):
         form.is_valid()
         record.delete()
         messages.success(request, "Deleted record.")
-        return redirect(reverse("tracker_detail", kwargs={'id': tracker.id}))
+        return redirect(reverse("tracker_detail", kwargs={"id": tracker.id}))
 
     return HttpResponse("Not implemented.")
 
@@ -127,44 +143,107 @@ def tracker_delete(request, id):
     return redirect("home")
 
 
-
 def dates_this_week():
     monday = datetime.date.today()
     while monday.isoweekday() != 1:
         monday = monday - datetime.timedelta(days=1)
-    dates= [monday + datetime.timedelta(days=i) for i in range(7)]
+    dates = [monday + datetime.timedelta(days=i) for i in range(7)]
     dates.sort()
     return dates
 
 
 def week_plot(request):
-    image = cache.get('week_plot')
+    image = cache.get("week_plot")
     if image:
         return HttpResponse(image, content_type="image/svg+xml")
 
     trackers = Tracker.objects.all()  # pylint: disable=no-member
+
     figure = Figure(figsize=(8, 2))
     ax = figure.subplots()
-
     width = 0.20
-    colors = cycle(('blue', 'green', 'red', 'purple'))
+    colors = cycle(("blue", "green", "red", "purple"))
 
     for i, t in enumerate(trackers):
         dates = dict()
         for d in dates_this_week():
             dates[d] = 0
 
-        records = Record.objects.all().filter(tracker=t, date__gte=min(dates.keys()))  # pylint: disable=no-member
+        records = Record.objects.all().filter(  # pylint: disable=no-member
+            tracker=t, date__gte=min(dates.keys())
+        )  
         for r in records:
             dates[r.date] += r.num_hours
-        
+
         dates, values = zip(*sorted(dates.items()))
-        dates = [d.strftime('%a') for d in dates]
-        locations = [x+width*i for x in range(7)]
-        ax.bar(locations, values, tick_label=dates, width=width, color=next(colors))  # pylint:disable=no-member
+        dates = [d.strftime("%a") for d in dates]
+        locations = [x + width * i for x in range(7)]
+        ax.bar(
+            locations, values, tick_label=dates, width=width, color=next(colors)
+        )  # pylint:disable=no-member
 
     figure.tight_layout()
     buf = BytesIO()
-    figure.savefig(buf, format='svg')
-    cache.set('week_plot', buf.getvalue())
+    figure.savefig(buf, format="svg")
+    cache.set("week_plot", buf.getvalue())
+    return HttpResponse(buf.getbuffer(), content_type="image/svg+xml")
+
+
+@require_GET
+def four_week_plot(request):
+    image = cache.get("four_week_plot")
+    if image:
+        return HttpResponse(image, content_type="image/svg+xml")
+
+    # find monday of this week
+    monday = datetime.date.today()
+    while monday.isoweekday() != 1:
+        monday -= datetime.timedelta(days=1)
+
+    # find monday five weeks ago
+    start = monday - datetime.timedelta(days=7 * 4)
+    end = start + datetime.timedelta(days=7)
+    data = dict()
+    trackers = Tracker.objects.all()  # pylint: disable=no-member
+
+    # collect data and store it in dict
+    for _ in range(4):
+        data[start] = dict()
+        for t in trackers:
+            # find sum of all records monday-sunday
+            total = (
+                Record.objects.all()  # pylint:disable=no-member
+                .filter(tracker=t)
+                .filter(date__gte=start)
+                .filter(date__lt=end)
+                .aggregate(Sum("num_hours"))
+            )
+            # record it in the range dictionary
+            data[start][t.name] = total
+        start += datetime.timedelta(days=7)
+        end += datetime.timedelta(days=7)
+
+    # create figure
+    figure = Figure(figsize=(8, 2))
+    ax = figure.subplots()
+    width = 0.20
+    colors = cycle(("blue", "green", "red", "purple"))
+
+    # plot the data dict
+    dates = list(sorted(data.keys()))
+    dates_pretty = [d.strftime("%b %d") for d in dates]
+    for i, t in enumerate(trackers):
+        values = []
+        for d in dates:
+            values.append(data[d][t.name]["num_hours__sum"] or 0)
+        locations = [x + width * i - width for x in range(4)]
+        ax.bar(
+            locations, values, tick_label=dates_pretty, width=width, color=next(colors)
+        )
+
+    # save the image in cache
+    figure.tight_layout()
+    buf = BytesIO()
+    figure.savefig(buf, format="svg")
+    cache.set("four_week_plot", buf.getvalue())
     return HttpResponse(buf.getbuffer(), content_type="image/svg+xml")
